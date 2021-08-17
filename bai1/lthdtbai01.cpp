0ed6fe1b745af9fd0f843eb5c678d396215309ec
DevDKhanh <duykhanh06.work@gmail.com>
2021-08-18 06:36:07 +0700

    init

---
+++ bai1/lthdtbai01.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <math.h>
+
+using namespace std;
+
+//===Chuong trinh chinh===
+int main()
+{   
+    //Khai bao bien
+    double a, b, c, p, dTich, cVi;
+
+    //Khai bao file de doc
+	ifstream fileInput("tamgiac.txt");
+
+    //Tao file ketqua-lthdtbai01.txt
+	ofstream fileOutput("ketqua-lthdtbai01.txt");
+
+	while (!fileInput.eof())
+	{
+		fileInput >> a;
+		fileInput >> b;
+		fileInput >> c;
+	}
+
+    cout << "Ba canh cua tam giac lan luot la: " << a << " " << b << " " << c << "\n";
+    fileOutput.clear();
+
+    //Tinh chu vi tam giac va luu vao file
+    cVi = a + b + c;
+    cout << "Chu vi tam giac la: " << cVi <<"\n";
+    fileOutput << "Chu vi tam giac la: " << cVi <<"\n";
+
+    //Tinh dien tich tam giac va luu vao file
+    p = cVi/2; //Nua chu vi tam giac
+    dTich = sqrt(p*(p-a)*(p-b)*(p-c));
+    cout << "Dien tich tam giac la: " << dTich << "\n";
+    fileOutput << "Dien tich tam giac la: " << dTich;
+
+    fileInput.close();
+    fileOutput.close();
+    
+    cout << "Luu ket qua vao file ketqua-lthdtbai01.txt \n"; 
+    cout << "Da luu thanh cong"; 
+
+	cout << endl;
+	return 0;
+}
